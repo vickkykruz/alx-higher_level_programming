b58f70736aa8056a48177f08bfd3f76665e2dbc5
Victor Chukwuemeka Edward <onwuegbuchulemvic02@gmail.com>
2023-08-12 14:48:37 -0700

    Modify Changes

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     lenght = len(my_list)
+    new_list = my_list
     # CHECK THE INDEX
-    if idx < 0 or idx > lenght:
-        return my_list
+    if idx < 0 or idx >= lenght:
+        return new_list
 
     # DELECT THE ITEX OF THE GIVEN INDEX
-    del my_list[idx]
-    return my_list
+    del new_list[idx]
+    return new_list
