cba6f7112306c4722cce6a79e5945d9483000781
Victor Chukwuemeka Edward <onwuegbuchulemvic02@gmail.com>
2023-08-12 13:06:20 -0700

    Lists of lists = Matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i, n in enumerate(row):
+            print("{:d}".format(n), end="")
+            if i < len(row) - 1:
+                print(" ", end="")
+        print()
