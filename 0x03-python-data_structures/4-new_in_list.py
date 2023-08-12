0a5de654f8f7763cfc97ce9746221106eefd760d
Victor Chukwuemeka Edward <onwuegbuchulemvic02@gmail.com>
2023-08-12 09:15:36 -0700

    Checking corretcion

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,6 +3,10 @@ def new_in_list(my_list, idx, element):
     new_list = my_list[:]
     lenght = len(new_list)
     # APPILY SOME CONDITIONS
-    if idx >= 0 and idx < lenght:
-        new_list[idx] = element
+    if idx < 0:
+        return my_list
+    elif idx >= lenght:
+        return my_list
+    # CHANGE THE ELEMENT IN THE POSITION
+    new_list[idx] = element
     return new_list
