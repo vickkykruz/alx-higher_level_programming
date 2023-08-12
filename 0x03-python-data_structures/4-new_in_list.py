df45083de736f19e287348116b3147eaa5f56709
Victor Chukwuemeka Edward <onwuegbuchulemvic02@gmail.com>
2023-08-12 12:22:39 -0700

    added the function .copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,16 +1,13 @@
 #!usr/bin/python3
 def new_in_list(my_list, idx, element):
-    new_list = []
-    lenght = len(my_list)
+    new_list = list.copy(my_list)
+    lenght = len(new_list)
     # CHECKING FOR THE CONDITION
     if idx < 0:
         return my_list
     elif idx >= lenght:
         return my_list
     else:
-        # APPEND THE ITEMS IN THE ORIGINAL LIST TO THE NEW LIST
-        for item in my_list:
-            new_list.append(item)
         # MODIFED THE ELEMENT OF THE INDEX
         new_list[idx] = element
         return new_list
