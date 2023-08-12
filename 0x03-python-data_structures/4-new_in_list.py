7e9bbdf6c80c5e8a571b8f8eb7ee8a7f969da754
Victor Chukwuemeka Edward <onwuegbuchulemvic02@gmail.com>
2023-08-12 12:27:25 -0700

    correction

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,12 +1,12 @@
 #!usr/bin/python3
 def new_in_list(my_list, idx, element):
     new_list = list.copy(my_list)
-    lenght = len(new_list)
+    lenght = len(new_list) - 1
     # CHECKING FOR THE CONDITION
     if idx < 0:
-        return my_list
-    elif idx >= lenght:
-        return my_list
+        return new_list
+    elif idx > lenght:
+        return new_list
     else:
         # MODIFED THE ELEMENT OF THE INDEX
         new_list[idx] = element
