93a18daf22461464a7f271bc6418ebf2920b1e9b
Victor Chukwuemeka Edward <onwuegbuchulemvic02@gmail.com>
2023-08-12 08:54:11 -0700

    correction

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,8 @@
 def new_in_list(my_list, idx, element):
     new_list = my_list[:]
     lenght = len(new_list)
+    # APPILY SOME CONDITIONS
     if idx < 0 or idx >= lenght:
-        return new_list
+        return my_list
     new_list[idx] = element
     return new_list
