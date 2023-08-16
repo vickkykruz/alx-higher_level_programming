a68a7ebff90a7c0a6fe4171ecaa050db8b03d459
Victor Chukwuemeka Edward <onwuegbuchulemvic02@gmail.com>
2023-08-16 06:14:39 -0700

    Change in task 1 file

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     # CHECK IF THE LIST IS NOT AN EMPTY LIST
+    """
     if len(my_list) == 0:
         return None
+    """
     new_list = list(map(lambda x: replace if x == search else x, my_list))
     return new_list
