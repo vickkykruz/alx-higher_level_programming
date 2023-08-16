98772ea4fe2774ea98aafe526808266caeb45de3
Victor Chukwuemeka Edward <onwuegbuchulemvic02@gmail.com>
2023-08-16 05:36:32 -0700

    Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-	return list(x ** 2 for row in matrix for x in row )
+    if len(matrix) == 0:
+        return None
+    else:
+        return list([x ** 2 for x in row] for row in matrix)
