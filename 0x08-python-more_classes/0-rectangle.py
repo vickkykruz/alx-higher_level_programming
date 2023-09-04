07caf95379eb21e58ff1d1b59fafb83bef998734
Victor Chukwuemeka Edward <onwuegbuchulemvic02@gmail.com>
2023-09-04 06:28:02 -0700

    Simple Rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    This is a rectangle module that define the class Rectangle
+"""
+
+
+class Rectangle:
+    """ This is an empty class Rectangle module
+    """
+
+    pass
