758cec9f4bce1013ab39442cb4b50531c7770997
Victor Chukwuemeka Edward <onwuegbuchulemvic02@gmail.com>
2023-09-12 05:23:03 -0700

    Read File

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    This is a ``read_file`` that reads a text file (UTF8) and print it to
+    stdout:
+
+        => Prototype: def read_file(filename=""):
+        => You must use the with statement.
+"""
+
+
+def read_file(filename=""):
+    """ This is a function that return printed text """
+
+    with open(filename, encoding="utf-8") as f:
+        print(f.read())
