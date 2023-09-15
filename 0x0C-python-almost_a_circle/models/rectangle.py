50afba80450eddfd29e7b50cfe775fcc25e1d146
Victor Chukwuemeka Edward <onwuegbuchulemvic02@gmail.com>
2023-09-15 06:17:36 -0700

    __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -83,6 +83,11 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
         self.__y = value
 
+    def __str__(self):
+        """ This is a method the return the string """
+        return "[Rectangle ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y,
+                self.__width, self.__height)
+
     def area(self):
         """ This is a method that return the area of a rectangle """
         return (self.__height * self.__width)
