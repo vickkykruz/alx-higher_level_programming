8befa74928503f7f75f1edb3d852a9d96e0bf358
vickkykruz <onwuegbuchulemvic02@gmail.com>
2023-10-18 09:37:38 -0700

    Made Changes

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,7 +2,7 @@
 -- states (in the database hbtn_0d_usa) on your MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS tablename (
+CREATE TABLE IF NOT EXISTS states (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(256) NOT NULL
 );
--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,13 +1,11 @@
 -- This is a SQL Script that creates the database hbtn_0d_usa and the table
--- cities (in the database hbtn_0d_usa) on your MySQL server.
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
--- Use the DATABASE
-USE hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 -- Create a TABLE
-CREATE TABLE IF NOT EXISTS cities (
-	id INT NOT NULL AUTO_INCREMEMT,
-	state_id INT NOT NULL,
-	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY (id),
-	FOREIGN KEY (state_id) REFERENCES states(id)
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
+	PRIMARY KEY (`id`),
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`state_id` INT NOT NULL,
+	`name` VARCHAR(256) NOT NULL,
+	FOREIGN KEY (`state_id`)
+	REFERENCES `hbtn_0d_usa`.`states`(`id`)
 );
