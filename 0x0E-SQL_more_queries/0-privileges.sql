10fa1d402c45008348a9a88150ad2060ad706bed
vickkykruz <onwuegbuchulemvic02@gmail.com>
2023-10-18 05:11:21 -0700

    Adjusted Tasks 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -2,4 +2,4 @@
 -- user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 -- This script also list all the privileges for user_0d_2
-SHOW GRANTS FOR 'user_0d_2'@'localhost'0;
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
