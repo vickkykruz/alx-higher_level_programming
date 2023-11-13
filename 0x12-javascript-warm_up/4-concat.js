946af7d2d16a270626e13168b5e979ee5ef3831e
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-11-13 07:36:41 -0800

    Create a sentence

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+/* Write a script that prints two arguments passed to it, in the following
+ * format: “ is ”
+ */
+
+const args = process.argv.slice(2);
+console.log(args[0] + ' is ' + args[1]);
