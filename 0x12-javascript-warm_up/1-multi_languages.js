ecdbf5027c3ff0b14ff8fb2d0837606afbc50254
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-11-13 06:06:50 -0800

    3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/* Write a script that prints 3 lines: */
+
+const firstLine = 'C is fun';
+const secondLine = 'Python is cool';
+const thirdLine = 'JavaScript is amazing';
+
+console.log(firstLine);
+console.log(secondLine);
+console.log(thirdLine);
