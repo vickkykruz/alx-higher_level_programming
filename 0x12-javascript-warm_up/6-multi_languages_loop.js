16b83a1225648f98ba757ec1a6508e5973f1a5a6
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-11-13 10:53:02 -0800

    Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+/* Write a script that prints 3 lines: (like 1-multi_languages.js) but by
+ * using an array of string and a loop
+ */
+
+const strArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const item of strArr) console.log(item);
