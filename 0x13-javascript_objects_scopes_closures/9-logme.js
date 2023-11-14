8ec3f4dd39e238835a0117b03bd329de0dc28bb8
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-11-14 11:10:15 -0800

    Log me

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/* Write a function that prints the number of arguments already printed and
+ * the new argument value.
+ */
+
+let counter = 0;
+
+exports.logMe = function (item) {
+  console.log(`${counter}: ${item}`);
+  counter++;
+};
