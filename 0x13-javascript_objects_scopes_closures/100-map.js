a90fa4a4b7d93c0153645c2a7adf7b0ae893dd0f
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-11-14 11:47:45 -0800

    Factory index

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+/* Write a script that imports an array and computes a new array. */
+
+const { list } = require('./100-data.js');
+console.log(list);
+console.log(list.map((item, index) => item * index));
