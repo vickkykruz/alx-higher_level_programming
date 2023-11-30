1fbc53f11c9deb8f4fe9e93d66d6766a646a1d29
vickkykruz <onwuegbuchulemvic02@gmail.com>
2023-11-30 08:21:02 -0800

    made adjustment according to the instruction

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Write a Bash script that takes in a URL, sends a request to that URL, and
-# displays the size of the body of the r
-
+# Write a Bash script that takes in a URL, displays the size of the body
 curl -s "$1" | wc -c
