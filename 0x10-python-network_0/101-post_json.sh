c8d9d4d5a701ba7b5d89d8cc1ab42cfb33a4724e
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-11-30 13:27:50 -0800

    correction

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Write a Bash script that sends a JSON POST displays the body of the response
-curl -s -X POST -H "Content-Type: application/json" -d "@json_file" "$1"
+curl -s -X POST -H "Content-Type: application/json" -d "$(cat $2" "$1"
