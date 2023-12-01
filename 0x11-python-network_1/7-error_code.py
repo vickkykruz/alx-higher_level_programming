a315f3a90e86fc46d125edd395ff00f4738eaad2
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2023-12-01 12:21:02 -0800

    Error code #1

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes in a URL, sends a request to the URL and
+displays the body of the response.
+"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    response = requests.get(url)
+
+    try:
+        print(response.text)
+    except HTTPError as err:
+        print(err.status_code)
