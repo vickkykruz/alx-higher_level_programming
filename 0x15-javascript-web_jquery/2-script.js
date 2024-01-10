8606079ed720201ced3946bb4fd684693fe6bdca
vickkykruz <onwuegbuchulemvic02@gmail.com>
2024-01-10 07:53:15 -0800

    Click and turn red

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,5 @@
+// Attach a click event handler to the DIV#red_header element
+$('#red_header').click(function() {
+      // Update the text color of the <header> element to red
+    $('header').css('color', '#FF0000');
+});
